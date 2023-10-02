4a262ca38494141224c825ff576d82e492dc3ca9
a.siverskii <a.siverskii@gmail.com>
2023-10-02 22:01:24 +0300

    add MessageUtils

---
+++ dispatcher/src/main/java/dev/crevan/utills/MessageUtils.java
@@ -0,0 +1,17 @@
+package dev.crevan.utills;
+
+import org.springframework.stereotype.Component;
+import org.telegram.telegrambots.meta.api.methods.send.SendMessage;
+import org.telegram.telegrambots.meta.api.objects.Update;
+
+@Component
+public class MessageUtils {
+
+    public SendMessage generateSendMessageWithText(final Update update, final String text) {
+        var message = update.getMessage();
+        var sendMessage = new SendMessage();
+        sendMessage.setChatId(message.getChatId().toString());
+        sendMessage.setText(text);
+        return sendMessage;
+    }
+}
