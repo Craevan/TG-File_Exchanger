85cead8bcdede27863f6f1f48cea129e5b41c192
a.siverskii <a.siverskii@gmail.com>
2023-10-17 22:45:45 +0300

    implement produce method

--- dispatcher/src/main/java/dev/crevan/service/impl/UpdateProducerImpl.java
+++ dispatcher/src/main/java/dev/crevan/service/impl/UpdateProducerImpl.java
@@ -2,6 +2,7 @@ package dev.crevan.service.impl;
 
 import dev.crevan.service.UpdateProducer;
 import lombok.extern.log4j.Log4j;
+import org.springframework.amqp.rabbit.core.RabbitTemplate;
 import org.springframework.stereotype.Service;
 import org.telegram.telegrambots.meta.api.objects.Update;
 
@@ -9,9 +10,15 @@ import org.telegram.telegrambots.meta.api.objects.Update;
 @Service
 public class UpdateProducerImpl implements UpdateProducer {
 
+    private final RabbitTemplate rabbitTemplate;
+
+    public UpdateProducerImpl(final RabbitTemplate rabbitTemplate) {
+        this.rabbitTemplate = rabbitTemplate;
+    }
+
     @Override
     public void produce(final String rabbitQueue, final Update update) {
         log.debug(update.getMessage().getText());
-        //TODO
+        rabbitTemplate.convertAndSend(rabbitQueue, update);
     }
 }
